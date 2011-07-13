c6334f3152e84c1fcf365601120c830bf92c04d0
Dave Ward <dave@encosia.com>
2011-07-13 19:44:13 -0400

    Check for existing widget data in mongo before requesting/parsing from jqueryui.com

--- query-jqui.js
+++ query-jqui.js
@@ -6,22 +6,44 @@ var Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
 
-var widgets = ['draggable', 'droppable', 'resizable', 'selectable'];
+var widgets = ['draggable', 'droppable', 'resizable', 'selectable', 'tabs'];
 							 //'sortable', 'accordion', 'autocomplete', 'button',
 							 //'datepicker', 'dialog', 'progressbar', 'slider',
 							 //'tabs', 'position'];
 
 for (var i = 0; i < widgets.length; i++) {
-  requestWidgetDocs(widgets[i], function(widget) {
-		persistWidget(widget);
+  ifWidgetNotPersisted(widgets[i], function(widgetName) {
+    sys.puts(widgetName + ' not persisted; accessing the tubes.');
+  
+    requestWidgetDocs(widgetName, function(widget) {
+      persistWidget(widget);
+    });
 	});
+}
+
+function ifWidgetNotPersisted(widgetName, callback) {
+  var db = new Db('jquery-ui-api', new Server('127.0.0.1', 27017, {}), { native_parser: true });
+  
+  db.open(function(err, db) {
+    db.collection('widgets', function(err, collection) {
+    
+      collection.find({ 'name' : widgetName }, function(err, cursor) {
+        cursor.toArray(function(err, docs) {
+          db.close();
+        
+          if (docs.length === 0)
+            callback.call(widgetName, widgetName);
+        });
+      });
+    });
+  });
 }
 
 function persistWidget(widget) {
   var db = new Db('jquery-ui-api', new Server('127.0.0.1', 27017, {}), { native_parser: true });
   
   db.open(function(err, db) {
-    db.collection(widget.name, function(err, collection) {
+    db.collection('widgets', function(err, collection) {
       collection.insert(widget);
       
       sys.puts('Inserted ' + widget.name);
