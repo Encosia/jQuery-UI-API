e07fe0daad79622bccd3442fd3690e578b78f3cf
Dave Ward <dave@encosia.com>
2011-07-14 02:28:32 -0400

    Express routes/config

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    api = require('./jquery-ui-api.js');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+app.get('/api/:widgetName', function(req, res) {
+  api.get_widgetDocs(req.params.widgetName, function(widget) {
+    res.contentType('json');
+
+    res.send(JSON.stringify(widget));
+  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
